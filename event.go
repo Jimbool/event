d663c9b39430b2819ab9e8e0e3118b62f112a61f
aeneasr <aeneas.rekkas@serlo.org>
2015-04-08 14:02:08 +0200

    Initial commit

---
+++ event.go
@@ -0,0 +1,43 @@
+package event
+
+type Listener interface {
+    Trigger(event string, data interface{})
+}
+
+type eventManager struct {
+    listeners map[string][]Listener
+}
+
+func New() *eventManager {
+    return &eventManager{
+        listeners: make(map[string][]Listener, 0),
+    }
+}
+
+func (em *eventManager) AttachListener(event string, listener Listener) {
+    if em.listeners[event] == nil {
+        em.listeners[event] = make([]Listener,0)
+    }
+    em.DetachListener(event, listener)
+    em.listeners[event] = append(em.listeners[event], listener)
+}
+
+func (em *eventManager) DetachListener(event string, listener Listener) {
+    if em.listeners[event] == nil {
+        return
+    }
+    for k, v := range em.listeners[event] {
+        if v == listener {
+            em.listeners[event] = append(em.listeners[event][:k], em.listeners[event][k+1:]...)
+        }
+    }
+}
+
+func (em *eventManager) Trigger(event string, data interface{}) {
+    if em.listeners[event] == nil {
+        return
+    }
+    for _, listener := range em.listeners[event] {
+        go listener.Trigger(event, data)
+    }
+}
