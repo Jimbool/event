8d952ce81ad1f96099dbc2b15c39a1633334763b
aeneasr <aeneas.rekkas@serlo.org>
2015-04-17 10:00:41 +0200

    Added ListenerAggregates

--- event_test.go
+++ event_test.go
@@ -1,78 +1,78 @@
 package event
 
 import (
-    "testing"
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
-type eventListener struct {}
+type eventListener struct{}
 
 const eventName = "foobar"
 
 type eventData struct {
-    t *testing.T
-    c chan bool
-    m string
+	t *testing.T
+	c chan bool
+	m string
 }
 
 func (l *eventListener) Trigger(event string, data interface{}) {
-    if d, ok := data.(*eventData); ok {
-        d.c <- true
-        return
-    }
-    panic("Type assertion failed")
+	if d, ok := data.(*eventData); ok {
+		d.c <- true
+		return
+	}
+	panic("Type assertion failed")
 }
 
-type dontListen struct {}
+type dontListen struct{}
 
 func (l *dontListen) Trigger(event string, data interface{}) {
-    if c, ok := data.(*eventData); ok {
-        c.t.FailNow()
-        return
-    }
-    panic("Type assertion failed")
+	if c, ok := data.(*eventData); ok {
+		c.t.FailNow()
+		return
+	}
+	panic("Type assertion failed")
 }
 
-type waitListener struct {}
+type waitListener struct{}
 
 func (l *waitListener) Trigger(event string, data interface{}) {
-    if c, ok := data.(*eventData); ok {
-        c.m = "foobar"
-        return
-    }
-    panic("Type assertion failed")
+	if c, ok := data.(*eventData); ok {
+		c.m = "foobar"
+		return
+	}
+	panic("Type assertion failed")
 }
 
 func TestTriggerAndWait(t *testing.T) {
-    data := &eventData{t, nil, ""}
-    em := New()
-    el := new(waitListener)
+	data := &eventData{t, nil, ""}
+	em := New()
+	el := new(waitListener)
 
-    em.AttachListener(eventName, el)
-    em.TriggerAndWait(eventName, data)
+	em.AttachListener(eventName, el)
+	em.TriggerAndWait(eventName, data)
 
-    assert.Equal(t, "foobar", data.m)
+	assert.Equal(t, "foobar", data.m)
 }
 
 func TestTrigger(t *testing.T) {
-    data := &eventData{t, make(chan bool), ""}
-    em := New()
-    el := new(eventListener)
+	data := &eventData{t, make(chan bool), ""}
+	em := New()
+	el := new(eventListener)
 
-    em.AttachListener(eventName, el)
-    em.Trigger(eventName, data)
+	em.AttachListener(eventName, el)
+	em.Trigger(eventName, data)
 
-    assert.True(t, <-data.c)
+	assert.True(t, <-data.c)
 }
 
 func TestDetach(t *testing.T) {
-    data := &eventData{t, make(chan bool), ""}
-    em := New()
-    el := new(dontListen)
+	data := &eventData{t, make(chan bool), ""}
+	em := New()
+	el := new(dontListen)
 
-    em.AttachListener(eventName, el)
-    em.DetachListener(eventName, el)
-    em.Trigger(eventName, data)
+	em.AttachListener(eventName, el)
+	em.DetachListener(eventName, el)
+	em.Trigger(eventName, data)
 
-    assert.True(t, true)
+	assert.True(t, true)
 }
